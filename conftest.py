750ea3577640973917db9b19b7182b1c8fc27bd3
HtestIs <kenjiyuuki1110.hutn@gmail.com>
2026-03-02 21:31:32 +0700

    day 8

--- conftest.py
+++ conftest.py
@@ -3,7 +3,9 @@ from driver_manager import DriverManager
 from utils.logger import setup_logger
 from utils.logger import test_filter
 import time
+from datetime import datetime
 import logging
+import os
 setup_logger()
 @pytest.hookimpl(tryfirst=True)
 def pytest_runtest_call(item):
@@ -16,7 +18,7 @@ def driver():
     yield driver
     driver.quit()
 logger = logging.getLogger(__name__)
-@pytest.hookimpl(hookwrapper=True)
+@pytest.hookimpl()
 def pytest_runtest_call(item):
     start_time = time.time()
     logger.info(f"===== START TEST: {item.name} =====")
@@ -33,3 +35,18 @@ def pytest_runtest_makereport(item, call):
             logger.error(f"===== TEST FAILED: {item.name} =====")
         elif result.passed:
             logger.info(f"===== TEST PASSED: {item.name} =====")
+def pytest_configure(config):
+    if not os.path.exists("reports"):
+        os.makedirs("reports")
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    report_file = f"reports/report_{timestamp}.html"
+    config.option.htmlpath = report_file
+    if config.pluginmanager.hasplugin("html"):
+        config._metadata = getattr(config, "_metadata", {})
+        config._metadata["Project Name"] = "MPOINTSHOP"
+        config._metadata["Tester"] = "Hieu"
+        config._metadata["Environment"] = "Chrome"
+        config.option.htmlpath = report_file
+def pytest_html_report_title(report):
+    report.title = "MPOINTSHOP Automation Report"
